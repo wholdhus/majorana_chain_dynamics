f5af2576f709d3465d9c1e5ba48dd7eef1f56bb9
Jack Harris <239680176+jack4harris@users.noreply.github.com>
2025-10-30 11:08:30 -0700

    Update O'Brien and Fendley Hamiltonians DMRG.jl

--- O'Brien and Fendley Hamiltonians DMRG.jl
+++ O'Brien and Fendley Hamiltonians DMRG.jl
@@ -6,20 +6,30 @@ let
 
     # Create the Hamiltonian of the Ising chain at its self-dual critical point
     # from the O'Brien and Fendley paper.
-    osI = OpSum()
+    os_I = OpSum()
     for j in 1:(N - 1)
-        osI += -2, "Sx", j # Not sure if this should be -1 or -2 as S=1/2 might make them 1/4 https://docs.itensor.org/ITensorMPS/stable/IncludedSiteTypes.html#%22Qubit%22-and-%22S1/2%22-Operators
-        osI += -4, "Sz", j, "Sz", j + 1 # Same here 
+        os_I -= "X", j 
+        os_I -= "Z", j, "Z", j + 1
     end
-    H_I = MPO(osI, sites)
+    # Boundary conditions
+    os_I -= "X", N # j = N
+    os_I -= "Z", N, "Z", 1
+
+    H_I = MPO(os_I, sites)
 
     # Create the Hamiltonian of the three-spin interaction from the O'Brien and Fendley paper.
-    os3 = OpSum()
+    os_3 = OpSum()
     for j in 1:(N - 2)
-        os3 += 8, "Sx", j, "Sz", j + 1, "Sz", j + 2
-        os3 += 8, "Sz", j, "Sz", j + 1, "Sx", j + 2
+        os_3 += "X", j, "Z", j + 1, "Z", j + 2
+        os_3 += "Z", j, "Z", j + 1, "X", j + 2
     end
-    H_3 = MPO(os3, sites)
+    # Boundary conditions
+    os_3 += "X", N - 1, "Z", N, "Z", 1 # j = N - 1
+    os_3 += "Z", N - 1, "Z", N, "X", 1
+    os_3 += "X", N, "Z", 1, "Z", 2 # j = N
+    os_3 += "Z", N, "Z", 1, "X", 2
+    
+    H_3 = MPO(os_3, sites)
 
     # Coupling coefficients
     lambdaI = 1
@@ -33,23 +43,41 @@ let
     # Making the initial energy offset a matrix product operator to match the Hamiltonians
     osE0 = OpSum()
     for j in 1:N
-        osE0 += E0, "Id", j   # "Id" makes this a MPO term apparently
+        osE0 += E0, "Id", j   # "Id" is the identity matrix
     end
     E0MPO = MPO(osE0, sites)
 
-    # Full Hamiltonian from paper
+    # Full Hamiltonian from the paper
     H = 2 * lambdaI * H_I + lambda3 * H_3 + E0MPO
 
     # Create an initial random matrix product state
-    psi0 = random_mps(sites)
-    nsweeps = 8
-    maxdim = [1, 2, 3, 10, 25, 50, 100, 500]
+    psi0 = random_mps(sites;linkdims=10)
+
+    # Sweeps and maximum dimensions
+    nsweeps = 10
+    maxdim = [5, 10, 25, 50, 100, 500, 1000]
     cutoff = 1.0e-10
 
-    # Run the DMRG algorithm, returning energy
-    # (dominant eigenvalue) and optimized MPS
-    energy, psi = dmrg(H, psi0; nsweeps, maxdim, cutoff)
-    println("Final energy = $energy")
+    # Outputs
+    energy1, psi1 = dmrg(H, psi0; nsweeps, maxdim, cutoff)
+    energy2, psi2 = dmrg(H, [psi1], psi0; nsweeps, maxdim, cutoff, weight = 100)
+    energy3, psi3 = dmrg(H, [psi1, psi2], psi0; nsweeps, maxdim, cutoff, weight = 100)
+    energy4, psi4 = dmrg(H, [psi1, psi2, psi3], psi0; nsweeps, maxdim, cutoff, weight = 100)
+    
+    println("Final energy 1 = $energy1")
+    println("Final energy 2 = $energy2")
+    println("Final energy 3 = $energy3")
+    println("Final energy 4 = $energy4")
+    println("<psi1|psi1> = $(inner(psi1, psi1))")
+    println("<psi2|psi2> = $(inner(psi2, psi2))")
+    println("<psi3|psi3> = $(inner(psi3, psi3))")
+    println("<psi4|psi4> = $(inner(psi4, psi4))")
+    println("<psi1|psi2> = $(inner(psi1, psi2))")
+    println("<psi1|psi3> = $(inner(psi1, psi3))")
+    println("<psi1|psi4> = $(inner(psi1, psi4))")
+    println("<psi2|psi3> = $(inner(psi2, psi3))")
+    println("<psi2|psi4> = $(inner(psi2, psi4))")
+    println("<psi3|psi4> = $(inner(psi3, psi4))")
 
     nothing
 end
