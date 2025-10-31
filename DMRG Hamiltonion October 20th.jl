e71ede01deb1a2ff7648876e47614720be3dac3d
Jack Harris <239680176+jack4harris@users.noreply.github.com>
2025-10-31 12:59:29 -0700

    Outputs in comments
    Outputs of the O'Brien Fendley model in the comments for cutoff = 1.0e-14

--- DMRG Hamiltonion October 20th.jl
+++ DMRG Hamiltonion October 20th.jl
@@ -36,7 +36,7 @@ let
     psi0 = random_mps(sites)
     nsweeps = 12
     maxdim = [10, 25, 50, 100, 250, 500, 1000, 2500, 5000]
-    cutoff = 1.0e-15
+    cutoff = 1.0e-12
 
     energy1, psi1 = dmrg(H, psi0; nsweeps, maxdim, cutoff)
     energy2, psi2 = dmrg(H, [psi1], psi0; nsweeps, maxdim, cutoff, weight = 100)
