3640f129036b1c8a40fa17cc40b54b58b6b6c17d
Will Holdhusen <wholdhus@gmail.com>
2026-04-06 18:16:14 -0700

    Moved MPO construction to its own file

--- dmrg_sim.jl
+++ dmrg_sim.jl
@@ -1,7 +1,27 @@
 using ITensors, ITensorMPS, HDF5
 using LinearAlgebra
 using YAML
-include("tebd_sim.jl")
+include("operators.jl")
+
+function entropy(state, bond; spectrum=false)
+	s_orth = orthogonalize(state, bond)
+	U,S,V = svd(s_orth[bond], 
+				(linkinds(s_orth, bond-1)..., 
+				 siteinds(s_orth, bond)...))
+	SvN = 0.0
+	ps = zeros(dim(S, 1))
+	for n=1:dim(S, 1)
+		p = S[n,n]^2
+		SvN -= p * log(p)
+		ps[n] = p
+	end
+	if spectrum
+		return SvN, ps
+	else
+		return SvN
+	end
+end
+
 
 function gs_dmrg(L, bc, parity,
                  t0, g;
